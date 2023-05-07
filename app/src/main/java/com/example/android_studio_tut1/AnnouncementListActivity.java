6086a3a383fab8c51e8d0d7bbe04f2e0d6f1128e
Batuhan Gülcemal <bthnglcml001@hotmail.com>
2023-05-07 10:39:34 +0300

    Announcement Duration

--- app/src/main/java/com/example/android_studio_tut1/AnnouncementListActivity.java
+++ app/src/main/java/com/example/android_studio_tut1/AnnouncementListActivity.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,17 +16,17 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
-import java.util.ArrayList;
+import java.util.Date;
 
 
 public class AnnouncementListActivity extends AppCompatActivity {
 
+    private final int announcementDurationDay = 5;
     FirebaseAuth auth;
     FirebaseFirestore db;
     FirebaseUser user;
@@ -63,30 +62,49 @@ public class AnnouncementListActivity extends AppCompatActivity {
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 Announcement announcement = document.toObject(Announcement.class);
 
-                                Button button = new Button(getApplicationContext());
-                                button.setText(announcement.announcement_title);
-                                button.setLayoutParams(new
-                                        LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                                        LinearLayout.LayoutParams.WRAP_CONTENT));
+                                //control if outdated announcement
+                                Date now = new Date(); // gets current date
+                                Date announcementDate = announcement.date;
+                                long difference_In_Time
+                                        = now.getTime() - announcementDate.getTime(); //milliseconds
+                                long difference_In_Days
+                                        = (difference_In_Time
+                                        / (1000 * 60 * 60 * 24))
+                                        % 365;
+                                if(difference_In_Days > announcementDurationDay){
+                                    //announcement is outdated
+                                }else{
+                                    Button button = new Button(getApplicationContext());
+                                    button.setText(announcement.announcement_title);
+                                    button.setLayoutParams(new
+                                            LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                                            LinearLayout.LayoutParams.WRAP_CONTENT));
 
-                                button.setOnClickListener(new View.OnClickListener()
-                                {
-                                    @Override
-                                    public void onClick(View view)
+                                    button.setOnClickListener(new View.OnClickListener()
                                     {
-                                        //Toast.makeText(AnnouncementListActivity.this, announcement.announcement_title, Toast.LENGTH_SHORT).show();
+                                        @Override
+                                        public void onClick(View view)
+                                        {
+                                            //Toast.makeText(AnnouncementListActivity.this, announcement.announcement_title, Toast.LENGTH_SHORT).show();
 
-                                        //start activity with parameter
-                                        Intent intent = new Intent(getApplicationContext(), AnnouncementActivity.class);
-                                        Bundle bundle = new Bundle();
-                                        bundle.putString("documentId", document.getId());
-                                        intent.putExtras(bundle);
-                                        startActivity(intent);
-                                        finish();
-                                    }
-                                });
+                                            //start activity with parameter
+                                            Intent intent = new Intent(getApplicationContext(), AnnouncementActivity.class);
+                                            Bundle bundle = new Bundle();
+                                            bundle.putString("documentId", document.getId());
+                                            intent.putExtras(bundle);
+                                            startActivity(intent);
+                                            finish();
+                                        }
+                                    });
 
-                                linearLayout.addView(button);
+                                    linearLayout.addView(button);
+                                }
+
+
+
+
+
+
 
                             }
                         } else {
@@ -95,7 +113,7 @@ public class AnnouncementListActivity extends AppCompatActivity {
                     }
                 });
 
-
-
     }
+
+
 }
